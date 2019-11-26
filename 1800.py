42dffd321bc62d10dda9ffa99e3add185663be48
Alexey Krivtsov <48096606+qurebeer@users.noreply.github.com>
2019-11-26 09:26:49 +0500

    Update 1800.py

--- 1800.py
+++ 1800.py
@@ -1,4 +1,15 @@
 import math
-t = math.sqrt((2 * h) / g) // время падения
-f = w * t // угол поворота за падение
-N = f / (2 * math.pi) //количество оборотов
+
+
+def main():
+    l, h, w = map(int, input().split())
+    print(check(l, h, w))
+
+def check(l, h, w):
+  g = 9.81
+  t = math.sqrt((2 * h) / g) // время падения
+  f = w * t // угол поворота за падение
+  N = f / (2 * math.pi) //количество оборотов
+
+if __name__ == '__main__':
+    main();
