b594ae0650f0d5e13ac42b8669036f853773212c
Alexey Krivtsov <48096606+qurebeer@users.noreply.github.com>
2019-11-26 22:56:23 +0500

    1800 (#1)

--- 1800.py
+++ 1800.py
@@ -1,15 +1,15 @@
 import math
 
-
 def main():
     l, h, w = map(int, input().split())
     print(check(l, h, w))
 
 def check(l, h, w):
-  g = 9.81
-  t = math.sqrt((2 * h) / g) // время падения
-  f = w * t // угол поворота за падение
-  N = f / (2 * math.pi) //количество оборотов
+    f = math.sqrt(h/l)
+    if math.copysign(1, math.sqrt(h/l)) == 1:
+        return("butter")
+    elif math.copysign(1, math.sqrt(h/l)) == -1:
+        return("bread")
 
 if __name__ == '__main__':
     main();
