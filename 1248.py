6286518f3dc367e1dd3a8902f782e22b450ee6d1
Alexey Krivtsov <48096606+qurebeer@users.noreply.github.com>
2019-12-10 18:45:17 +0500

    1248 (#6)

---
+++ 1248.py
@@ -0,0 +1,15 @@
+import math
+
+
+def main():
+    posled = []
+    summ = 0
+    N = int(input())
+    for i in range(N):
+        posled.append(float(input()))
+
+    print("{:.19e}".format(sum(posled)))
+
+
+if __name__ == '__main__':
+    main()
